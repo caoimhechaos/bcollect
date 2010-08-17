400e1f3582fe48b18a8472c19eaff6ab37c8ec71
Caoimhe Chaos <caoimhechaos@protonmail.com>
2010-08-18 01:14:10 +0200

    Change my mail address to the ancient-solutions.com one.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # (c) 2008, Caoimhe Chaos <caoimhechaos@protonmail.com>,
-#	    SyGroup GmbH Basel. All rights reserved.
+#	    Ancient Solutions. All rights reserved.
 #
 # Redistribution and use  in source and binary forms,  with or without
 # modification, are  permitted provided that  the following conditions
@@ -13,7 +13,7 @@
 #   notice, this  list of conditions  and the following  disclaimer in
 #   the  documentation  and/or   other  materials  provided  with  the
 #   distribution.
-# * Neither  the  name  of  the  SyGroup  GmbH nor  the  name  of  its
+# * Neither  the  name  of  Ancient Solutions  nor  the  name  of  its
 #   contributors may  be used to  endorse or promote  products derived
 #   from this software without specific prior written permission.
 #
@@ -28,8 +28,6 @@
 # STRICT  LIABILITY,  OR  TORT  (INCLUDING  NEGLIGENCE  OR  OTHERWISE)
 # ARISING IN ANY WAY OUT OF  THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# $SyGroup: scripts/soprop/backup_mysqldb/autogen.sh,v 1.1 2008-10-10 07:53:49 caoimhe Exp $
 #
 
 aclocal
